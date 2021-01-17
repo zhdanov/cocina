a98b68c8b338d5868bd3df558fa706ce020ebd35
zhdanov <yuriy.zhdanov@gmail.com>
2021-01-17 23:59:55 +0300

    feat(setup): gitlab clone cocina-backend

--- .helm/postdeploy.bash
+++ .helm/postdeploy.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo sed -i -e '/^.*cocina-dev.loc\.loc.*$/d' /etc/hosts
-echo `minikube ip`" cocina-dev.loc" | sudo tee -a /etc/hosts
+sudo sed -i -e "/^.*cocina-$shortenv\.loc.*$/d" /etc/hosts
+echo `minikube ip`" cocina-$shortenv.loc" | sudo tee -a /etc/hosts
---
+++ container-post-start.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone git@gitlab-$ENVIRONMENT.gitlab-$ENVIRONMENT.svc.cluster.local:zhdanov/cocina-backend.git /root/cocina-backend;
+
+# mv /root/cocina to /var/www
