a6ee2f5fc3dbd9a8628bc6414a4a5a4cb2d5c28c
zhdanov <yuriy.zhdanov@gmail.com>
2021-02-26 22:17:25 +0300

    refactor(build): remove frontend directory

--- container-post-start.bash
+++ container-post-start.bash
@@ -5,11 +5,31 @@ then
 
     printf "Host gitlab-prod.gitlab-prod\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile=/dev/null\n" >> /etc/ssh/ssh_config
 
-    if [[ ! -d /var/www/cocina-backend ]]; then
+    if [[ ! -d /var/www/cocina-backend/upload ]]; then
+        if [[ -d /var/www/cocina-backend ]]; then
+          rmdir /var/www/cocina-backend
+        fi
+
         git clone git@gitlab-prod.gitlab-prod:$HOME_USER_NAME/cocina-backend.git /var/www/cocina-backend
-        pushd /var/www/cocina-backend/
-            ln -s /root/cocina/frontend .
+
+        mkdir -p /var/www/cocina-backend/frontend
+        pushd /var/www/cocina-backend/frontend
+            ln -s /root/cocina/common.blocks .
+            ln -s /root/cocina/desktop.blocks .
+            ln -s /root/cocina/mobile.blocks .
+            ln -s /root/cocina/mobile-landscape.blocks .
+            ln -s /root/cocina/config.css .
         popd
+
+        mkdir -p /var/www/cocina-backend/storage
+        chmod -R 777 /var/www/cocina-backend/storage
+
+        mkdir -p /var/www/cocina-backend/upload/image/cache
+        chmod -R 777 /var/www/cocina-backend/upload/image/cache
     fi
+
+    pushd /root/cocina/hakunamatata
+        npm i
+    popd
 
 fi
