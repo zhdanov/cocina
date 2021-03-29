c8ca2c5c64a85c2a5b9c7426ab49107fe1422216
zhdanov <yuriy.zhdanov@gmail.com>
2021-03-29 21:51:43 +0300

    fix(build): post start container

--- container-post-start.bash
+++ container-post-start.bash
@@ -5,27 +5,29 @@ then
 
     printf "Host gitlab-prod.gitlab-prod\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile=/dev/null\n" >> /etc/ssh/ssh_config
 
-    if [[ ! -d /var/www/cocina-backend/upload ]]; then
-        if [[ -d /var/www/cocina-backend ]]; then
-          rmdir /var/www/cocina-backend
-        fi
+    if [[ ! -d /var/www/cocina-backend/.git ]]; then
 
-        git clone git@gitlab-prod.gitlab-prod:$HOME_USER_NAME/cocina-backend.git /var/www/cocina-backend
+        pushd /var/www/cocina-backend/
 
-        mkdir -p /var/www/cocina-backend/frontend
-        pushd /var/www/cocina-backend/frontend
-            ln -s /root/cocina/common.blocks .
-            ln -s /root/cocina/desktop.blocks .
-            ln -s /root/cocina/mobile.blocks .
-            ln -s /root/cocina/mobile-landscape.blocks .
-            ln -s /root/cocina/config.css .
+            git init
+            git remote add origin git@gitlab-prod.gitlab-prod:$HOME_USER_NAME/cocina-backend.git
+            git pull origin master
+
+            mkdir -p /var/www/cocina-backend/frontend
+            pushd /var/www/cocina-backend/frontend
+                ln -s /root/cocina/common.blocks .
+                ln -s /root/cocina/desktop.blocks .
+                ln -s /root/cocina/mobile.blocks .
+                ln -s /root/cocina/mobile-landscape.blocks .
+                ln -s /root/cocina/config.css .
+            popd
+
+            mkdir -p /var/www/cocina-backend/storage
+            chmod -R 777 /var/www/cocina-backend/storage
+
+            mkdir -p /var/www/cocina-backend/upload/image/cache
+            chmod -R 777 /var/www/cocina-backend/upload/image/cache
         popd
-
-        mkdir -p /var/www/cocina-backend/storage
-        chmod -R 777 /var/www/cocina-backend/storage
-
-        mkdir -p /var/www/cocina-backend/upload/image/cache
-        chmod -R 777 /var/www/cocina-backend/upload/image/cache
     fi
 
     pushd /root/cocina/hakunamatata
