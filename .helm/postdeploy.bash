683c368996e60634bb0aa3595982ad99d5c1cebb
zhdanov <yuriy.zhdanov@gmail.com>
2021-03-29 23:02:02 +0300

    refactor(helm): common templates

--- .helm/postdeploy.bash
+++ .helm/postdeploy.bash
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-./../hakunamatata/container/container__make-alias.bash cocina-dev php-fpm
-./../hakunamatata/container/container__copy-dotfiles.bash cocina-dev php-fpm
+pushd "$(dirname "$0")"
+
+    ./../hakunamatata/container/container__make-alias.bash cocina-dev php-fpm
+    ./../hakunamatata/container/container__copy-dotfiles.bash cocina-dev php-fpm
+
+popd
