d2e4d645b3a2be8dd77697f46123615945f4e1cf
zhdanov <yuriy.zhdanov@gmail.com>
2021-07-02 17:35:47 +0000

    build(container): clean

--- container-post-start.bash
+++ container-post-start.bash
@@ -13,15 +13,6 @@ then
             git remote add origin git@gitlab-prod.gitlab-prod:$HOME_USER_NAME/cocina-backend.git
             git pull origin master
 
-            mkdir -p /var/www/cocina-backend/frontend
-            pushd /var/www/cocina-backend/frontend
-                ln -s /root/cocina/common.blocks .
-                ln -s /root/cocina/desktop.blocks .
-                ln -s /root/cocina/mobile.blocks .
-                ln -s /root/cocina/mobile-landscape.blocks .
-                ln -s /root/cocina/config.css .
-            popd
-
             mkdir -p /var/www/cocina-backend/storage
             chmod -R 777 /var/www/cocina-backend/storage
 
